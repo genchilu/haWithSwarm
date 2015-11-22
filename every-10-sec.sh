fae76b6c31e0eb700fa64c78d8516a6e6885d49b
Genchi <genchilu@gmail.com>
2015-11-23 00:47:34 +0800

    Remove duplicate script

--- every-10-sec.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-while true
-do
- bash /git/hademo/checkAlive.sh
- sleep 10
-done
--- scaleup.sh
+++
@@ -1,2 +0,0 @@
-docker -H 10.62.41.134:2376 run -d -p 3000:3000 --restart=always --entrypoint=/usr/bin/node genchilu/helloweb /opt/helloweb/app.js
-bash /git/hademo/updateConsul.sh
